aebbce17300c554ae9f583002dd120c1d84cdcae
Asher Spurr <asher.spurr@gmail.com>
2024-01-10 17:42:13 -0700

    Create player objects and toggle their turns

--- main.js
+++ main.js
@@ -1,5 +1,62 @@
+/* 
 
 
-/*
-Goal
 */
+/* JavaScript */
+var players = [
+    {
+        name: 'player1',
+        icon: '🌿',
+        isTurn: false,
+        hasWon: false,
+        id: Date.now(),
+    },
+    {
+        name: 'player2',
+        icon: '🐛',
+        isTurn: true,
+        hasWon: false,
+        id: Date.now(),
+    }
+]
+
+var gameplay = []
+
+/* Changes isTurn to true or false each time it's called */
+function changeTurn() {
+    for(var i = 0; i < players.length; i++) {
+        players[i].isTurn = !players[i].isTurn
+        renderTurnIcon()
+    }
+}
+
+function trackScore() {
+    
+}
+
+/* HTML */
+var turns = document.querySelector('.turns')
+var gameBoard = document.querySelector('.gameboard-container')
+
+gameBoard.addEventListener('click', (e) => {
+    renderCellIcon(e)
+});
+
+function renderCellIcon(event) {
+    var event = event.target.closest('div')
+    for(var i = 0; i < players.length; i++) {
+        if(players[i].isTurn === true) {
+            event.innerText = `${players[i].icon}`
+            // event.id = players[i].id
+            }
+        }
+        changeTurn()
+    }
+
+function renderTurnIcon() {
+    for(var i = 0; i < players.length; i++) {
+        if(players[i].isTurn === true) {
+         turns.innerText = `It's ${players[i].icon}'s turn`
+        }
+    }
+}
